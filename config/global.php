333c2defda7cbc8d8266dfc2cb00c228b3598a7b
Valentina <valentinas792@gmail.com>
2020-06-04 21:37:38 -0500

    Inicio del proyecto. Creación de los archivos de configuración

---
+++ config/global.php
@@ -0,0 +1,6 @@
+<?php
+// Creación de constantes globales para poder acceder a ellas desde cualquier sitio.
+define("CONTROLADOR_DEFECTO", "Usuarios");
+define("ACCION_DEFECTO", "index");
+//Más constantes de configuración
+?>
